ad1bcbc5cc7c06eb4b583eadacfc4b18c042e73c
mwz73trz <michaelzura@bellsouth.net>
2021-10-17 11:27:53 -0400

    backend gtg

--- backend/mlb_proj/settings.py
+++ backend/mlb_proj/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'coresheaders',
+    'corsheaders',
     'mlb_auth',
     'mlb_app',
 ]
--- backend/mlb_proj/urls.py
+++ backend/mlb_proj/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('mlb_app.urls')),
-    path('login', include('mlb_auth.urls')),
+    path('login/', include('mlb_auth.urls')),
 ]
